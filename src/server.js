13ea7028784649bd5c0a7b70660c8e2f272f3863
Philipp Daniels <philipp.daniels@gmail.com>
2016-03-12 16:31:22 +0100

    Integrate mongonse as MongoDB interface

--- src/server.js
+++ src/server.js
@@ -1,7 +1,23 @@
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://db/meetat', function(err) {
+  if(err) {
+    console.log('connection error', err);
+  } else {
+    console.log('connection successful');
+  }
+});
+var UserSchema = new mongoose.Schema({
+  nickname: String,
+  phone_number: String,
+  created_at: { type: Date, default: Date.now },
+});
+var User = mongoose.model('User', UserSchema);
+
+
 var express = require('express');
 var app = express();
 
-app.get('/', function (req, res) {
+app.post('/', function (req, res) {
   res.send('Hello World!');
 });
 
